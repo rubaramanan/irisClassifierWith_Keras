2910aa06da792ac2e354ff3c0496ca3703fa8d66
ThayaRamanan <rubaramanan1997@gmail.com>
2019-11-29 00:52:24 +0530

    first

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from tensorflow import keras
+
+model = keras.models.load_model('mymodel.h5')
+
+test = ([[5.9,3.0,5.1,1.8]])
+
+result = np.argmax(model.predict(test))
+
+iris_labes=['Iris-setosa','Iris-versicolor','Iris-virginica']
+
+print(iris_labes[result])
